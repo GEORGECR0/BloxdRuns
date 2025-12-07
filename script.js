380a5cde9385676c71fdf5a610751d4dd5e8edfb
GEORGECR0 <151453431+GEORGECR0@users.noreply.github.com>
2025-12-07 22:30:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+  const buttons = document.querySelectorAll(".nav-btn");
+
+  buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+      document.querySelector(".nav-btn.active")?.classList.remove("active");
+      btn.classList.add("active");
+    });
+  });
+
+const puter = new Puter();
+
+document.getElementById("googleLogin").onclick = async () => {
+  try {
+    const user = await puter.auth.signInWithOAuth("google", {
+      clientId: "813275968419-0hleihr80b9fktej5n07locu40ofm4fr.apps.googleusercontent.com"
+    });
+    console.log("Logged in user:", user);
+    alert(`Hello, ${user.name || "User"}!`);
+  } catch (err) {
+    console.error("Login failed:", err);
+    alert("Login failed. See console for details.");
+  }
+};
